4b4f002f1b3fbb2fa0e243f8b244e9302d908d50
Ta-ven <576321399@qq.com>
2019-10-13 22:50:35 +0800

    update .gitignore

---
+++ spider/test.py
@@ -0,0 +1,23 @@
+from spider import Spider
+from manager.run import run
+
+
+class MySpider(Spider):
+    def __init__(self):
+        super(MySpider, self).__init__()
+        self.dbname = "test"
+        self.table_name = "test"
+        self.is_purge = True
+
+    def start_produce(self):
+        for i in ["https://www.baidu.com/s?wd=%E4%BE%8B%E5%AD%90", "https://www.baidu.com/", ]:
+            yield {"url":i}
+
+    def parse(self, res):
+        print(res.status_code)
+        print(res.text)
+        print()
+
+if __name__ == '__main__':
+    run("test.py", function="w")
+
